6a610403927fd375ae1ea10118f4895ac094193b
Anugoon <ton25sanook@gmail.com>
2019-02-21 16:12:03 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Form from './components/Form.vue'
 import Admin from './components/Admin.vue'
+import GetUser from './components/GetUser.vue'
 
 const routes = [
     {path: '/', component: Form, name: 'Form'},
-    {path: '/admin', component: Admin, name: 'admin'}
+    {path: '/admin', component: Admin, name: 'admin'},
+    {path: '/GetUser',component: GetUser, name: 'GetUser'}
 ]
 
 export default routes
